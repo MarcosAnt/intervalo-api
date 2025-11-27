e464a81deea118d27fb09c677ea3ce3bf0106419
Marcos Antonio <m-antonio198@hotmail.com>
2025-11-27 18:52:47 -0300

    Adicionado mais testes

--- src/test/java/com/intervalo/intervalo_api/controller/WinnerIntervalControllerTest.java
+++ src/test/java/com/intervalo/intervalo_api/controller/WinnerIntervalControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -22,5 +23,28 @@ public class WinnerIntervalControllerTest {
         mockMvc.perform(get("/api/intervals"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void deveRetornarIntervaloCorreto() throws Exception {
+        mockMvc.perform(get("/api/intervals"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.min").exists())
+                .andExpect(jsonPath("$.max").exists());
+    }
+
+    @Test
+    void deveRetornarValoresCorretosParaIntervalo() throws Exception {
+        mockMvc.perform(get("/api/intervals"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.min[0].producer").value("Produtor A"))
+                .andExpect(jsonPath("$.min[0].interval").value(1))
+                .andExpect(jsonPath("$.min[0].previousWin").value(2000))
+                .andExpect(jsonPath("$.min[0].followingWin").value(2001))
+                .andExpect(jsonPath("$.max[0].producer").value("Produtor B"))
+                .andExpect(jsonPath("$.max[0].interval").value(10))
+                .andExpect(jsonPath("$.max[0].previousWin").value(1990))
+                .andExpect(jsonPath("$.max[0].followingWin").value(2000));
+    }
+
 
 }
